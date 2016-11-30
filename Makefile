0454ddbc392a30fd8c22efddb4fd94e22e19d743
Simon Ask Ulsnes <simon@ulsnes.dk>
2016-11-30 12:07:05 +0100

    Realm networking based test (C++)

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .DEFAULT_GOAL := all
 all: test.cpp asio.cpp Makefile
-	# clang++ -std=c++14 -I $(REALM_SYNC_PREFIX)/src -I $(REALM_CORE_PREFIX)/src -o irontest-cpp -O0 -g test.cpp http-parser/http_parser.o -L$(REALM_SYNC_PREFIX)/src/realm -L$(REALM_CORE_PREFIX)/src/realm -lrealm-sync -lrealm $(LDFLAGS)
-	clang++ -std=c++14 -O3 -fno-omit-frame-pointer -g -I/usr/local/include asio.cpp http-parser/http_parser.o -o irontest-asio
+	$(CXX) -std=c++14 -I $(REALM_SYNC_PREFIX)/src -I $(REALM_CORE_PREFIX)/src -o irontest-cpp -O3 -g test.cpp http-parser/http_parser.o -L$(REALM_SYNC_PREFIX)/src/realm -L$(REALM_CORE_PREFIX)/src/realm -lrealm-sync -lrealm $(LDFLAGS)
+	$(CXX) -std=c++14 -O3 -g -I/usr/local/include asio.cpp http-parser/http_parser.o -o irontest-asio
 clean:
 	rm -f irontest-cpp
--- test.cpp
+++ test.cpp
@@ -2,68 +2,172 @@
 #include "http-parser/http_parser.h"
 
 #include <iostream>
+#include <vector>
+#include <memory>
+#include <sstream>
+#include <cstring>
 
 using namespace realm;
 using namespace realm::util;
 
 struct Server {
+    static constexpr size_t buffer_size = 1024;
+
     network::Service service;
     network::Acceptor acceptor;
     network::Socket client;
     network::Endpoint endpoint;
 
-    Server() : acceptor(service), client(service) {}
+    struct Client {
+        Server& server;
+        network::Socket socket;
+        std::unique_ptr<char[]> recv_buffer;
+        std::string send_buffer;
+        bool responding = false;
+        http_parser parser;
+        http_parser_settings settings;
 
-    void start(std::string listen_addr, std::string listen_port) {
-        network::Resolver resolver(service);
-        network::Resolver::Query query(listen_addr, listen_port, network::Resolver::Query::passive | network::Resolver::Query::address_configured);
-        network::Endpoint::List endpoints;
-        resolver.resolve(query, endpoints);
-        network::Acceptor acceptor(service);
-        auto i = endpoints.begin(); // FIXME: Always listens on first endpoint
-        std::error_code ec;
-        for (; i != endpoints.end(); ++i) {
-            acceptor.open(i->protocol(), ec);
-            if (!ec) {
-                acceptor.set_option(network::SocketBase::reuse_address(true), ec);
-                if (!ec) {
-                    acceptor.bind(*i, ec);
-                    if (!ec) {
-                        break;
-                    }
-                }
-                acceptor.close();
-            }
-        }
-        if (ec) {
-            std::cerr << "Error binding acceptor: " << ec << "\n";
-            return;
-        }
-        endpoint = *i;
-        acceptor.listen();
+        explicit Client(Server& server);
+        void keep_reading();
+        void close();
+        void on_message_complete();
+        void on_headers_complete();
+        void on_url(const char* url, size_t len);
 
+        static int on_message_complete(http_parser*);
+        static int on_headers_complete(http_parser*);
+        static int on_url(http_parser*, const char*, size_t);
+    };
+
+    std::unique_ptr<Client> next_client;
+    std::vector<std::unique_ptr<Client>> clients;
+
+    Server() : acceptor(service), client(service) {
+        acceptor.open(network::StreamProtocol::ip_v6());
+        acceptor.bind(network::Endpoint(network::StreamProtocol::ip_v6(), 3005));
+    }
+
+    void start() {
         std::cout << "Listening on port " << acceptor.local_endpoint().port() << "...\n";
-        service.post([]() { std::cout << "Hello :)\n"; });
-        service.run();
+        acceptor.listen(10);
         initiate_accept();
+        service.run();
         std::cout << "run() returned\n";
     }
 
     void initiate_accept() {
-        acceptor.async_accept(client, endpoint, [this](auto ec) {
-            if (ec == util::error::operation_aborted) {
+        next_client = std::make_unique<Client>(*this);
+        acceptor.async_accept(next_client->socket, [&](auto ec) {
+            if (ec == error::operation_aborted) {
                 std::cerr << "aborted\n";
                 return;
             }
-            std::cout << "accepted\n";
-            client.close();
+            else if (ec) {
+                std::cerr << "accept() error: " << ec.message() << "\n";
+                return;
+            }
+            next_client->keep_reading();
+            this->clients.push_back(std::move(next_client));
             this->initiate_accept();
         });
     }
 };
 
+Server::Client::Client(Server& server) : server(server), socket(server.service), recv_buffer(new char[buffer_size]) {
+    http_parser_init(&parser, HTTP_REQUEST);
+    parser.data = this;
+    http_parser_settings_init(&settings);
+    settings.on_message_complete = &Client::on_message_complete;
+    settings.on_url = &Client::on_url;
+    settings.on_headers_complete = &Client::on_headers_complete;
+}
+
+void Server::Client::keep_reading() {
+    socket.async_read_some(recv_buffer.get(), buffer_size, [this](std::error_code ec, size_t len) {
+        if (ec == error::operation_aborted) {
+            close();
+            return;
+        }
+        if (len == 0) {
+            http_parser_execute(&parser, &settings, nullptr, 0);
+            close();
+            return;
+        }
+        if (!ec) {
+            http_parser_execute(&parser, &settings, recv_buffer.get(), len);
+            if (!responding)
+                keep_reading();
+        }
+        else {
+            std::cerr << "socket error: " << ec.message() << "\n";
+            close();
+        }
+    });
+}
+
+void Server::Client::on_message_complete() {
+    responding = true;
+    const char body[] = "Hello, World!";
+    std::stringstream ss;
+    ss << "HTTP/1.1 200 OK\r\n"
+        "Server: Realm\r\n"
+        "Content-Length: " << std::strlen(body) << "\r\n"
+        "Content-Type: text/plain\r\n"
+        "\r\n" << body;
+    send_buffer = ss.str();
+    socket.async_write(send_buffer.data(), send_buffer.size(), [this](std::error_code ec, size_t len) {
+        responding = false;
+        if (ec == error::operation_aborted) {
+            close();
+            return;
+        }
+        if (ec) {
+            std::cerr << "socket error (send): " << ec.message() << "\n";
+        }
+        if (http_should_keep_alive(&parser)) {
+            keep_reading();
+        }
+        else {
+            close();
+        }
+    });
+}
+
+void Server::Client::on_headers_complete() {
+    // std::cout << "on_headers_complete()\n";
+}
+
+void Server::Client::on_url(const char* url, size_t len) {
+    // std::cout << "on_url: " << std::string(url, len) << "\n";
+}
+
+int Server::Client::on_message_complete(http_parser* parser) {
+    static_cast<Client*>(parser->data)->on_message_complete();
+    return 0;
+}
+
+int Server::Client::on_headers_complete(http_parser* parser) {
+    static_cast<Client*>(parser->data)->on_headers_complete();
+    return 0;
+}
+
+int Server::Client::on_url(http_parser* parser, const char* url, size_t len) {
+    static_cast<Client*>(parser->data)->on_url(url, len);
+    return 0;
+}
+
+void Server::Client::close() {
+    server.service.post([this]() {
+        Server& server = this->server;
+        auto it = std::find_if(begin(server.clients), end(server.clients), [this](auto& a) { return a.get() == this; });
+        if (it != end(server.clients)) {
+            server.clients.erase(it); // suicide
+        }
+    });
+}
+
 int main(int, char**) {
     Server server;
-    server.start("127.0.0.1", "3004");
+    server.start();
 }
 
